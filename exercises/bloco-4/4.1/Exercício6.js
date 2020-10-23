711c8deea21bb381671fee38cea3805abd742a53
Nícolas Godek <nicolasgodek@gmail.com>
2020-10-23 20:47:43 -0300

    execício 8

--- exercises/bloco-4/4.1/Exercício6.js
+++ exercises/bloco-4/4.1/Exercício6.js
@@ -1,18 +1,16 @@
-let pecaXadrez = "peão";
-
-pecaXadez = Lcase(pecaXadrez);
+let pecaXadrez = "PEÃO";
 
 if (pecaXadrez == "rei"){
     console.log("Pode andar na horizontal, vertical e diagonal. Porém somente uma casa");
-}else if (pecaXadrez == "rainha"){
+}else if (pecaXadrez.toLowerCase() == "rainha"){
     console.log("Pode andar na horizontal, vertical e diagonal.");
-}else if (pecaXadrez == "bispo"){
+}else if (pecaXadrez.toLowerCase() == "bispo"){
     console.log("Pode andar na diagonal");
-}else if (pecaXadrez == "torre"){
+}else if (pecaXadrez.toLowerCase() == "torre"){
     console.log("Pode andar na vertical e horizontal");
-}else if (pecaXadrez ==  "cavalo"){
+}else if (pecaXadrez.toLowerCase()==  "cavalo"){
     console.log("Pode andar em L");
-}else if (pecaXadrez == "peão"){
+}else if (pecaXadrez.toLowerCase() == "peão"){
     console.log("Pode andar somente para frente, duas ou uma casa se for a primeira jogada e somente uma casa depois. Além de só poder comer na digonal com somente uma casa de distância");
 }else{
     console.log("Não exite essa peça");
---
+++ exercises/bloco-4/4.1/Exercício8.js
@@ -0,0 +1,9 @@
+let a = 6;
+let b = 3;
+let c = 7;
+
+if (a % 2 || b % 2 || c % 2){
+    console.log(true)
+}else{
+    console.log(false)
+}
