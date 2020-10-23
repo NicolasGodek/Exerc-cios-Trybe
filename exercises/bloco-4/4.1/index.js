b6b0e112073b8bf5e21536386512eb0f32a2ca91
Nícolas Godek <nicolasgodek@gmail.com>
2020-10-23 17:55:48 -0300

    execício 3 e 4

--- exercises/bloco-4/4.1/index.js
+++ exercises/bloco-4/4.1/index.js
@@ -2,6 +2,10 @@ let a = 7;
 
 let b = 2;
 
+let c = 9;
+
+let d = -8;
+
 let adicao = a + b;
 
 let subtracao = a - b;
@@ -27,3 +31,20 @@ if (a > b) {
 }else {
     console.log("b é maior que a");
 }
+
+if (a > b && a > c){
+    console.log("a é o maior número");
+}else if (b > a && b > c){
+    console.log("b é o maior número");
+}else{
+    console.log("c é o maior número");
+}
+
+if (d > 0){
+    console.log("d é positivo");
+}else if (d < 0){
+    console.log("d é negativo");
+}else{
+    console.log("d é igual a 0");
+}
+
