f0e4581fc3b4f09d01e100e0f2a0766a2a63c1b9
Nícolas Godek <nicolasgodek@gmail.com>
2020-10-23 21:03:57 -0300

    execício 9

--- exercises/bloco-4/4.1/Exercício8.js
+++ exercises/bloco-4/4.1/Exercício8.js
@@ -1,8 +1,8 @@
-let a = 6;
-let b = 3;
+let a = 1;
+let b = 2;
 let c = 7;
 
-if (a % 2 || b % 2 || c % 2){
+if (a % 2 == 0 || b % 2 == 0|| c % 2 == 0){
     console.log(true)
 }else{
     console.log(false)
