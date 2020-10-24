bc5f82cc6df4fe70fd33d5b4ab8350e568c10475
Nícolas Godek <nicolasgodek@gmail.com>
2020-10-23 21:13:18 -0300

    execício 10

---
+++ exercises/bloco-4/4.1/Exercício10.js
@@ -0,0 +1,13 @@
+let custo = 10;
+
+let valorDeVenda = 15;
+
+let valorDeImposto = 0.2 * custo;
+
+let valorDeCustoTotal = custo + valorDeImposto;
+
+let lucro = valorDeVenda - valorDeCustoTotal;
+
+let lucroTotal = lucro * 1000;
+
+    console.log(lucroTotal)
