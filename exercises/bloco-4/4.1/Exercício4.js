d905475c311c030f9f3fbcb79b6fa0b07b8789c8
Nícolas Godek <nicolasgodek@gmail.com>
2020-10-30 20:01:16 -0300

    exercício de hoje

--- exercises/bloco-4/4.1/Exercício4.js
+++ exercises/bloco-4/4.1/Exercício4.js
@@ -1,9 +1,12 @@
-let d = -8;
-
-if (d > 0){
-    console.log("d é positivo");
-}else if (d < 0){
-    console.log("d é negativo");
-}else{
-    console.log("d é igual a 0");
+function verificacaoDoNumero(x){
+    if (x > 0){
+        verificacaoDoNumero = "O número é positivo";
+    }else if (x < 0){
+        verificacaoDoNumero = "O número é negativo";
+    }else{
+        verificacaoDoNumero = "O número é igual a 0";
+    };
+    return verificacaoDoNumero;
 }
+
+console.log(verificacaoDoNumero(0));
